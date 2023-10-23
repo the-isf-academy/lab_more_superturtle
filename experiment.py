ceb9475414f76f3f722aae7d0b0734e487ee5c50
Emma <eqbrown@isf.edu.hk>
2023-10-24 07:40:23 +0800

    added starter code for experiment

--- experiment.py
+++ experiment.py
@@ -1,3 +1,6 @@
+# this code has been adapted from the offical superturtle documentation
+# https://superturtle.readthedocs.io/en/latest/introduction.html
+
 from turtle import *
 from superturtle.animation import *
 from superturtle.movement import *
@@ -6,10 +9,28 @@ from random import randint
 
 colormode(255)
 
+rectangle_size = 60
+triangle_size = 30
 
-for frame in animate(frames=120, loop=True):
+for frame in animate(60, loop=True):
+    with frame.rotate(0, 360):
+        penup()
+        forward(100)
+        pendown()
+        rectangle(rectangle_size,rectangle_size, 'dark sea green')
 
 
+        with frame.rotate(0, 360, cycles=2):
+            penup()
+            forward(100)
+            pendown()
+            triangle(triangle_size,'coral')
 
+        with frame.rotate(0, 360, cycles=1):
+            penup()
+            forward(100)
+            right(45)
+            pendown()
+            triangle(triangle_size,'coral')
 
 
